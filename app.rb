f9588685b7a003104a8cd34bceed77049dd56177
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-10-17 12:45:24 +0300

    Completed assignment

---
+++ app.rb
@@ -0,0 +1,22 @@
+#app.rb
+#puts 'hello world'
+# puts "Hello Maria!"
+# puts "How are you?"
+# puts "Hello world!"
+
+#puts method
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# puts [1, 2, 3]
+# p [1, 2, 3]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+# #print method
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
